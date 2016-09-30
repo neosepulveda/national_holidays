43d50e0192cd37a541415ede58274bcf65d2ddb6
neosepulveda <israel.sepulveda.castillejo@gmail.com>
2016-09-30 16:56:32 +0100

    Addition of Norway

--- lib/national_holidays.rb
+++ lib/national_holidays.rb
@@ -2,16 +2,21 @@ require "national_holidays/version"
 require 'national_holidays/country_national_holidays'
 require 'national_holidays/countries/italy'
 require 'national_holidays/countries/united_kingdom'
+require 'national_holidays/countries/norway'
 
 module NationalHolidays
 
   def self.available_countries
     { "United Kingdom" => Countries::UnitedKingdom.new.regions,
-      "Italy" => Countries::Italy.new.regions }
+      "Italy" => Countries::Italy.new.regions,
+      "Norway" => Countries::Norway.new.regions
+    }
   end
 
   def self.country(country)
     case country
+    when 'Norway'
+      Countries::Norway.new
     when 'Italy'
       Countries::Italy.new
     when 'United Kingdom'
