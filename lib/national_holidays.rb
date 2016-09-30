acd20c7113f88c6e3a425afca172aaf1bf6ce256
neosepulveda <israel.sepulveda.castillejo@gmail.com>
2016-09-30 18:12:04 +0100

    Added United States file and the region of Alabama

--- lib/national_holidays.rb
+++ lib/national_holidays.rb
@@ -3,13 +3,15 @@ require 'national_holidays/country_national_holidays'
 require 'national_holidays/countries/italy'
 require 'national_holidays/countries/united_kingdom'
 require 'national_holidays/countries/norway'
+require 'national_holidays/countries/united_states'
 
 module NationalHolidays
 
   def self.available_countries
     { "United Kingdom" => Countries::UnitedKingdom.new.regions,
       "Italy" => Countries::Italy.new.regions,
-      "Norway" => Countries::Norway.new.regions
+      "Norway" => Countries::Norway.new.regions,
+      "United States" => Countries::UnitedStates.new.regions
     }
   end
 
@@ -21,6 +23,8 @@ module NationalHolidays
       Countries::Italy.new
     when 'United Kingdom'
       Countries::UnitedKingdom.new
+    when 'United States'
+      Countries::UnitedStates.new
     else
       Countries::UnitedKingdom.new
     end
