9de493ce150f096f4a1d6689589a5d886ed03284
neosepulveda <israel.sepulveda.castillejo@gmail.com>
2016-09-30 13:10:36 +0100

    Another bug fixed

--- lib/public_holidays/country_public_holidays.rb
+++ lib/public_holidays/country_public_holidays.rb
@@ -12,16 +12,16 @@ module PublicHolidays
       self.send(region.to_s)
     end
 
-    def public_holidays_month(year = Date.today.year, month = Date.today.month)
-      self.public_holidays_year(year).select { |ph| (ph.start_date.month == month) || (ph.end_date.month == month) }
+    def public_holidays_month(year = Date.today.year, month = Date.today.month, region = 'all')
+      self.public_holidays_year(year, region).select { |ph| (ph.start_date.month == month) || (ph.end_date.month == month) }
     end
 
-    def public_holidays_year(year = Date.today.year)
-      self.public_holidays.select { |ph| (ph.start_date.year == year) || (ph.end_date.year == year) }
+    def public_holidays_year(year = Date.today.year, region = 'all')
+      self.public_holidays(region).select { |ph| (ph.start_date.year == year) || (ph.end_date.year == year) }
     end
 
-    def is_public_holiday?(date = Date.today)
-      self.public_holidays.select { |ph| ph.is_public_holiday?(date) }.any?
+    def is_public_holiday?(date = Date.today, region = 'all')
+      self.public_holidays(region).select { |ph| ph.is_public_holiday?(date) }.any?
     end
 
     def regions
