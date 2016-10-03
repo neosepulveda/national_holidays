80ec47dfc6f97522d2a0fa74cc0d68596716cb10
neosepulveda <israel.sepulveda.castillejo@gmail.com>
2016-10-03 12:47:58 +0100

    Small refactor

--- lib/national_holidays.rb
+++ lib/national_holidays.rb
@@ -1,14 +1,6 @@
 require "national_holidays/version"
 require 'national_holidays/country_national_holidays'
-require 'national_holidays/countries/italy'
-require 'national_holidays/countries/united_kingdom'
-require 'national_holidays/countries/norway'
-require 'national_holidays/countries/united_states'
-require 'national_holidays/countries/portugal'
-require 'national_holidays/countries/france'
-require 'national_holidays/countries/ireland'
-require 'national_holidays/countries/austria'
-require 'national_holidays/countries/australia'
+require 'national_holidays/countries'
 
 module NationalHolidays
 
---
+++ lib/national_holidays/countries.rb
@@ -0,0 +1,17 @@
+require 'national_holidays/countries/italy'
+require 'national_holidays/countries/united_kingdom'
+require 'national_holidays/countries/norway'
+require 'national_holidays/countries/united_states'
+require 'national_holidays/countries/portugal'
+require 'national_holidays/countries/france'
+require 'national_holidays/countries/ireland'
+require 'national_holidays/countries/austria'
+require 'national_holidays/countries/australia'
+
+module NationalHolidays
+  module Countries
+    def self.test
+      "TEST"
+    end
+  end
+end
