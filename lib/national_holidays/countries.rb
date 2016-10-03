f9ef6156fff230339ea27d28bb2ac16f4c5e3ecd
neosepulveda <israel.sepulveda.castillejo@gmail.com>
2016-10-03 16:22:21 +0100

    Launchers improved and added more functions to countries

--- lib/national_holidays/countries.rb
+++ lib/national_holidays/countries.rb
@@ -10,8 +10,49 @@ require 'national_holidays/countries/australia'
 
 module NationalHolidays
   module Countries
-    def self.test
-      "TEST"
+
+    def self.holidays_country_or_region(str)
+      if str.nil?
+        nil
+      elsif str.empty? || str.downcase == 'default'
+        nil
+      elsif self.countries.include?(str.downcase.tr(" ", "_"))
+        self.country(str).region('Default')
+      else
+        self.region(str)
+      end
     end
+
+    def self.countries
+      ["australia", "austria", "france", "ireland", "italy", "norway", "portugal", "united_kingdom", "united_states"]
+    end
+
+    def self.country(country)
+      eval(self.to_class_format(country)).new
+    end
+
+    def self.region(region)
+      reg = self.to_human_format(region)
+      country = self.reverse_search(reg)
+
+      self.country(country).region(reg) unless country.nil?
+    end
+
+    def self.reverse_search(region)
+      country = self.countries_and_regions.select { |country, regions| regions.include?(region) }.keys.first
+    end
+
+    def self.countries_and_regions
+      self.countries.map { |country| [ country, eval(self.to_class_format(country)).new.regions ] }.to_h
+    end
+
+    def self.to_class_format(str)
+      str.tr("_", " ").split.map(&:capitalize).join('')
+    end
+
+    def self.to_human_format(str)
+      str.tr("_", " ").split.map(&:capitalize).join(' ')
+    end
+
   end
 end
