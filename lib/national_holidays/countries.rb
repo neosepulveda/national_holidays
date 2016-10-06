e3a5b480992ac4ab78123f4e6add4e3475c9fbf7
neosepulveda <israel.sepulveda.castillejo@gmail.com>
2016-10-06 12:36:16 +0100

    Denmark added

--- lib/national_holidays/countries.rb
+++ lib/national_holidays/countries.rb
@@ -19,6 +19,7 @@ require 'national_holidays/countries/china'
 require 'national_holidays/countries/colombia'
 require 'national_holidays/countries/croatia'
 require 'national_holidays/countries/czech_republic'
+require 'national_holidays/countries/denmark'
 
 module NationalHolidays
   module Countries
@@ -35,7 +36,7 @@ module NationalHolidays
 
     def self.countries
       [ "angola", "australia", "austria", "belgium", "brazil", "canada", "china",
-        "colombia", "croatia", "czech_republic", "ethiopia", "france", "germany",
+        "colombia", "croatia", "czech_republic", "denmark", "ethiopia", "france", "germany",
         "india", "ireland", "italy", "kenya", "norway", "portugal", "united_kingdom",
         "united_states"
       ]
