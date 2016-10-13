7ab84bc9a3567e7488abd39b935afcc59a001982
neosepulveda <israel.sepulveda.castillejo@gmail.com>
2016-10-13 15:26:07 +0100

    regional_national_holidays range method

--- lib/national_holidays/region.rb
+++ lib/national_holidays/region.rb
@@ -15,5 +15,12 @@ module NationalHolidays
       self.regional_national_holidays.select { |nh| nh.is_national_holiday?(date) }.any?
     end
 
+    def regional_national_holidays_range(args = { start_date: Date.today, end_date: Date.today })
+      args[:start_date] = Date.today if args[:start_date].nil?
+      args[:end_date] = Date.today if args[:end_date].nil?
+
+      self.regional_national_holidays.select { |nh| (nh.start_date >= args[:start_date]) && (nh.start_date <= args[:end_date]) }
+    end
+
   end
 end
